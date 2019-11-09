a67e1379d24105121f979625736c400d32fc5c58
naibo-code <naibo.ha@gmail.com>
2019-11-09 21:08:48 +0900

    Add tests.

---
+++ tests/test_test.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def test_demo():
+    from examples import demo
+
+
+def test_demo_custom_scalars():
+    from examples import demo_custom_scalars
+
+
+def test_demo_matplotlib():
+    from examples import demo_matplotlib
